7dc19aa0368bdf27ce0b016ab20c74211b8ffd19
Divinechizoba <chu21012@byui.edu>
2023-01-17 04:55:00 +0100

    chamber of commerce website

---
+++ chamber/scripts/index.js
@@ -0,0 +1,7 @@
+const d = new Date();
+let year = d.getFullYear(); 
+document.getElementById("demo").innerHTML = year;
+
+let oLastModif = document.lastModified;
+document.getElementById("lastupdated").innerHTML = oLastModif;
+
