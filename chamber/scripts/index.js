619da2d20679a8db213329a50f7859ed0e3db193
Divinechizoba <chu21012@byui.edu>
2023-02-14 07:24:16 +0100

    join page completed

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -5,3 +5,13 @@ document.getElementById("demo").innerHTML = year;
 let oLastModif = document.lastModified;
 document.getElementById("lastupdated").innerHTML = oLastModif;
 
+<form>
+  <input type="hidden" id="load-time" name="load-time" value=""></input>
+</form>
+
+// get the current date and time
+var now = new Date();
+// format the date and time as a string
+var dateTimeString = now.toISOString();
+// set the value of the hidden field to the date and time string
+document.getElementById("load-time").value = dateTimeString;
