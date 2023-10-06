937c898c96d44c2bb59c356829908efbcd64cd20
Divinechizoba <chu21012@byui.edu>
2023-10-06 06:54:08 +0100

    form page

--- script/getDates.js
+++ script/getDates.js
@@ -13,5 +13,24 @@ hambutton.addEventListener('click', () => {
     hambutton.classList.toggle('show');
 });
 
+function checkPasswordMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var message = document.getElementById("passwordMatchMessage");
+
+    if (password !== confirmPassword) {
+        message.innerHTML = "Passwords do not match.";
+        document.getElementById("password").value = "";
+        document.getElementById("confirmPassword").value = "";
+        document.getElementById("password").focus();
+    } else {
+        message.innerHTML = "";
+    }
+}
+
+function updateRatingValue() {
+    var rating = document.getElementById("rating").value;
+    document.getElementById("ratingValue").textContent = rating;
+}
 
 
